1b1381a1b0fbae429903ace1ee31063ed228947d
Mohith Jothi Kannan <jothiteenu@gmail.com>
2023-07-13 17:02:47 -0400

    header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,13 +1,11 @@
 import React from "react";
+import Logo from "../../resources/iquadra.png";
 
 const Header = () => {
   return (
-    <div className="header bg-[#043d5d] justify-between flex items-center rounded shadow padding p-5">
-      <div className="logo text-white w-1/4 items-center">
-        <img className=""
-          src="https://pbs.twimg.com/profile_images/1012281639953629185/xTmSYT09_400x400.jpg"
-          alt=""
-        />
+    <div className="header bg-[#043d5d] justify-between flex items-center rounded shadow padding p-10">
+      <div className="logo text-white w-1/4 p-10">
+        <img className="rounded-3xl h-{20}" src={Logo} alt="logo.png" />
       </div>
       <div className="headerTitle w-3/4 text-center">
         <h1 className="font-sans font-bold text-blue-300 text-4xl">

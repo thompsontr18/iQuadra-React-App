c0e4836fc1652e48c1098a9059f5f2faebd92b03
Mohith Jothi Kannan <jothiteenu@gmail.com>
2023-07-13 16:38:38 -0400

    resources

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,24 @@
-import React from 'react';
+import React from "react";
 
-const Header=()=>{
-    return(
-        <div>Header</div>
-    );
+const Header = () => {
+  return (
+    <div className="header bg-[#043d5d] justify-between flex items-center rounded shadow padding p-5">
+      <div className="logo text-white w-1/4 items-center">
+        <img className=""
+          src="https://pbs.twimg.com/profile_images/1012281639953629185/xTmSYT09_400x400.jpg"
+          alt=""
+        />
+      </div>
+      <div className="headerTitle w-3/4 text-center">
+        <h1 className="font-sans font-bold text-blue-300 text-4xl">
+          iQuadra Marketing
+        </h1>
+        <p className="font-sans text-white">
+          A Portal For Marketing Team To Access Web Scrapped Data
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default Header;

7116c19dfd645fd7a2ba2ac9bc3f4ed8e167ec56
Mohith Jothi Kannan <jothiteenu@gmail.com>
2023-07-13 12:49:17 -0400

    header footer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import React from "react";
 import { ReactDOM } from "react";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <p>Hi</p>
+      <Router>
+        <Header/>
+        {/* <div className="container">
+          <Routes>
+            <Route path={process.env.PUBLIC_URL} exact Component={Home} />
+            <Route path={process.env.PUBLIC_URL+"movie/:imdbID"} Component={MoveDetails} />
+            <Route path={process.env.PUBLIC_URL+"*"} Component={PageNotFound} />
+          </Routes>
+        </div> */}
+        <Footer/>
+      </Router>
     </div>
   );
 }

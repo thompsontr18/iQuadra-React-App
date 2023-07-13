d9e0d6fdbc2d672d55c2dcda083016d37ae232fb
thompsontr18 <thompsontr18@gmail.com>
2023-07-13 17:03:17 -0400

    footer update

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -10,31 +10,32 @@ import { FaArrowUp } from 'react-icons/fa';
 const Footer=()=>{
     return(
         <div>
+            <br></br>
             <footer className="footer bg-[#043d5d] rounded shadow p-5">
                 <div className="outerDiv w-full max-w-screen-xl p-4 ">
                     <div className="innerDiv sm:flex sm:items-center sm:justify-between">
                         <a className="logo flex items-center mb-4 sm:mb-0" href="https://iquadra.com/" target="_blank">
-                            <img src={logo} alt="Logo" className="logo h-8 mr-3"/>
-                            <span className="companyName self-center text-2xl font-semibold whitespace-nowrap text-white ">iQuadra</span>
+                            <img src={logo} alt="Logo" className="logo h-12 mr-3 mb-4 rounded"/>
+                            <span className="companyName font-sans font-bold text-blue-300 text-3xl mb-6 ">iQuadra Information Services</span>
                         </a>
-                        <ul className="socialMedias flex flex-wrap items-center mb-6 text-sm font-medium">
+                        <ul className="socialMedias flex flex-wrap items-center mb-4 text-sm font-medium">
                             <li>
-                                <a href="http://twitter.com/IquadraISLLC" className="mr-10 hover:opacity-50 flex items-center mt-4" target="_blank">
+                                <a href="http://twitter.com/IquadraISLLC" className="mr-12 hover:opacity-50 flex items-center mt-4" target="_blank">
                                 <FaTwitter className="twitterLogo fill-white" size="2em"/>
                                 </a>
                             </li>
                             <li>
-                                <a href="https://www.facebook.com/iQuadraInformationServices/" className="mr-10 hover:opacity-50 flex items-center mt-4" target="_blank">
+                                <a href="https://www.facebook.com/iQuadraInformationServices/" className="mr-12 hover:opacity-50 flex items-center mt-4" target="_blank">
                                 <FaFacebookF className="facebookLogo fill-white" size="2em"/>
                                 </a>
                             </li>
                             <li>
-                                <a href="https://www.linkedin.com/company/iquadra/mycompany/" className="mr-10 hover:opacity-50 flex items-center mt-4" target="_blank">
+                                <a href="https://www.linkedin.com/company/iquadra/mycompany/" className="mr-12 hover:opacity-50 flex items-center mt-4" target="_blank">
                                 <FaLinkedinIn className="linkedinLogo fill-white" size="2em"/>
                                 </a>
                             </li>
                             <li>
-                                <a href="#" className="mr-10 hover:opacity-50 flex items-center mt-4">
+                                <a href="#" className="mr-12 hover:opacity-50 flex items-center mt-4">
                                 <FaArrowUp className="backToTop fill-white" size="2em"/>
                                 </a>
                             </li>

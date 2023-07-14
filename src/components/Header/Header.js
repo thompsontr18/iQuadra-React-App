ed8902b2ad9c20315c03216fe8661578c566f021
Mohith Jothi Kannan <jothiteenu@gmail.com>
2023-07-14 12:30:30 -0400

    finished header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,15 +4,15 @@ import Logo from "../../resources/iquadra.png";
 const Header = () => {
   return (
     <div className="header bg-[#043d5d] justify-between flex items-center rounded shadow padding px-10">
-      <div className="logo text-white w-1/4 p-10">
-        <img className="rounded-3xl h-auto" src={Logo} alt="logo.png" />
+      <div className="logo flex justify-center items-center w-1/4 p-10">
+        <img className="w-20 h-20 rounded-xl" src={Logo} alt="logo.png" />
       </div>
       <div className="headerTitle w-3/4 text-center">
         <h1 className="font-sans font-bold text-blue-300 text-4xl">
           iQuadra Colleges Marketing Portal
         </h1>
         <p className="font-sans text-white">
-          A Portal For Marketing Team To Access Web Scrapped Data by the tech support team
+          A Portal For Marketing Team To Access The Web Scrapped Data From The Database
         </p>
       </div>
     </div>

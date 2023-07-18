226a8cd2f3a4f0743729f68049c4d539cdc676c6
thompsontr18 <thompsontr18@gmail.com>
2023-07-18 16:10:55 -0400

    bug testing

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -5,54 +5,38 @@ const Home=()=>{
         <div>
             <br></br>
             <div className="row float-left w-full">
-                <div className="leftColumn float-left w-4/12 border-2 border-[#043d5d] rounded shadow p-5">
+                <div className="leftColumn float-left w-4/12 border-2 border-[#043d5d] rounded shadow px-2">
                     <h1 className="font-sans text-white font-bold text-center text-xl">Filters</h1>
-
                     <div className="firstForm">
-                    <br></br><br></br><br></br><br></br><br></br>
 
+                        <div>
+                            <div class="container rounded-lg">
+                                <form>
+                                    <div class="sm:flex items-center bg-white rounded-lg overflow-hidden px-2 py-1">
+                                        <div class="py-1">
+                                            <select id="options" class="text-[#5A5A5A] outline-none border-2 border-[#D3D3D3] bg-[#D3D3D3] rounded-lg mr-4">
+                                                <option value="Select" disabled selected >Select</option>
+                                                <option value="Colleges">Colleges</option>
+                                                <option value="Majors">Majors</option>
+                                                <option value="Courses">Courses</option>
+                                                <option value="Professors">Professors</option>
+                                            </select>
+                                            <input class="text-base text-[#043d5d] outline-none  mr-6" type="text" placeholder="Search" />
+                                            <button class="border-[#D3D3D3] bg-[#D3D3D3] text-[#5A5A5A] text-base rounded-lg px-2 py-1">Search</button>
+                                        </div>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+
+                    <br></br><br></br><br></br><br></br><br></br>
                     </div>
                 </div>
 
 
+
                 <div className="rightColumn float-right w-8/12 border-2 border-[#043d5d] rounded shadow p-5">
                     <h1 className="font-sans text-white font-bold text-center text-xl">Results</h1>
-
-                    <form>
-                        <div class="flex">
-                            <label for="search-dropdown" class="mb-2 text-sm font-medium text-gray-900 sr-only"></label>
-                            <button id="dropdown-button" data-dropdown-toggle="dropdown" class="flex-shrink-0 z-10 inline-flex items-center py-2.5 px-4 text-sm font-medium text-center text-gray-900 bg-gray-100 border border-gray-300 rounded-l-lg hover:bg-gray-200 focus:ring-4 focus:outline-none focus:ring-gray-100" type="button">Select <svg class="w-2.5 h-2.5 ml-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
-                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4"/>
-                            </svg></button>
-                            <div id="dropdown" class="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44">
-                                <ul class="py-2 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdown-button">
-                                <li>
-                                    <button type="button" class="inline-flex w-full px-4 py-2 hover:bg-gray-100">Colleges</button>
-                                </li>
-                                <li>
-                                    <button type="button" class="inline-flex w-full px-4 py-2 hover:bg-gray-100">Majors</button>
-                                </li>
-                                <li>
-                                    <button type="button" class="inline-flex w-full px-4 py-2 hover:bg-gray-100">Courses</button>
-                                </li>
-                                <li>
-                                    <button type="button" class="inline-flex w-full px-4 py-2 hover:bg-gray-100">Professors</button>
-                                </li>
-                                </ul>
-                            </div>
-                            <div class="relative w-full">
-                                <input type="search" id="search-dropdown" class="block p-2.5 w-full z-20 text-sm text-gray-900 bg-gray-50 rounded-r-lg border-l-gray-50 border-l-2 border border-gray-300 focus:ring-blue-500 focus:border-blue-500" placeholder="Search" required></input>
-                                <button type="submit" class="absolute top-0 right-0 p-2.5 text-sm font-medium h-full text-white bg-blue-700 rounded-r-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300">
-                                    <svg class="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
-                                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
-                                    </svg>
-                                    <span class="sr-only">Search</span>
-                                </button>
-                            </div>
-                        </div>
-                    </form>
-
-
                 </div>
             </div>
         </div>

38112593aa4b8dc77785c00e64fbdfca15f056a3
Mohith Jothi Kannan <jothiteenu@gmail.com>
2023-07-27 19:24:40 -0400

    implemented multiple search

--- client/src/features/recordSlice.js
+++ client/src/features/recordSlice.js
@@ -11,53 +11,94 @@ export const fetchAsyncRecords = createAsyncThunk(
   }
 );
 
-export const fetchSearchRecords=createAsyncThunk(
-  "records/fetchSearchRecords",
-  async (value) => {
-    // console.log("Inside Slice");
-    // console.log(value);
-    // console.log(`http://localhost:8800/search?Category=${value.Category}&SearchFeild=${value.SearchFeild}`);
-    const res = await axios.get(`http://localhost:8800/search?Category=${value.Category}&SearchFeild=${value.SearchFeild}`).catch((err)=>{
-      console.log(err);
-    });
-    return res.data;
+export const fetchOriginalRecords = createAsyncThunk(
+  "records/fetchOriginalRecords",
+  async (value, thunkAPI) => {
+    return thunkAPI.getState().records.records;
   }
-)
+);
+
+export const fetchSearchInput = createAsyncThunk(
+  "records/fetchSearchInput",
+  async (value, thunkAPI) => {
+    const data = thunkAPI.getState().records.records;
+    console.log(value);
+    return data.filter((item) => {
+      return value.CollegeSearch === ""
+        ? item
+        : item.Colleges.includes(value.CollegeSearch);
+    }).filter((item) => {
+      return value.MajorsSearch === ""
+        ? item
+        : item.Majors.includes(value.MajorsSearch);
+    }).filter((item) => {
+      return value.CourseSearch === ""
+        ? item
+        : item.Courses.includes(value.CourseSearch);
+    }).filter((item) => {
+      return value.PersonSearch === ""
+        ? item
+        : item.Details.includes(value.PersonSearch);
+    });
+  }
+);
 
 const initialState = {
   records: [],
+  display: [],
 };
 
 const recordSlice = createSlice({
   name: "records",
   initialState,
-  reducers: {
-    
+  reducers: {},
+  extraReducers: {
+    [fetchAsyncRecords.pending]: () => {
+      console.log("Pending");
+    },
+    [fetchAsyncRecords.fulfilled]: (state, { payload }) => {
+      console.log("Fetched Successfully");
+      return { ...state, records: payload, display: payload };
+    },
+    [fetchAsyncRecords.rejected]: () => {
+      console.log("rejected");
+    },
+    // [fetchSearchRecords.pending]: () => {
+    //   console.log("Pending Query");
+    // },
+    // [fetchSearchRecords.fulfilled]: (state, { payload }) => {
+    //   console.log("Fetched Query Successfully");
+    //   return { ...state, records: payload };
+    // },
+    // [fetchSearchRecords.rejected]: () => {
+    //   console.log("Rejected");
+    // },
+    [fetchOriginalRecords.pending]: () => {
+      console.log("Pending");
+    },
+    [fetchOriginalRecords.fulfilled]: (state, { payload }) => {
+      console.log("Fetched Originals Successfully");
+      return { ...state, display: payload };
+    },
+    [fetchOriginalRecords.rejected]: (state, action) => {
+      console.log("rejected");
+      console.log(action.error);
+    },
+    [fetchSearchInput.pending]: () => {
+      console.log("Pending");
+    },
+    [fetchSearchInput.fulfilled]: (state, { payload }) => {
+      console.log("Fetched Colleges Successfully");
+      console.log(payload.length);
+      return { ...state, display: payload };
+    },
+    [fetchSearchInput.rejected]: (state, action) => {
+      console.log("Colleges rejected ");
+      console.log(action.error);
+    },
   },
-  extraReducers:{
-    [fetchAsyncRecords.pending]:()=>{
-        console.log("Pending");
-    },
-    [fetchAsyncRecords.fulfilled]:(state,{payload})=>{
-        console.log("Fetched Successfully");
-        return {...state,records: payload}
-    },
-    [fetchAsyncRecords.rejected]:()=>{
-        console.log("rejected");
-    },
-    [fetchSearchRecords.pending]:()=>{
-      console.log("Pending Query");
-    },
-    [fetchSearchRecords.fulfilled]:(state, {payload})=>{
-      console.log("Fetched Query Successfully");
-      return {...state, records:payload}
-    },
-    [fetchSearchRecords.rejected]:()=>{
-      console.log("Rejected");
-    }
-  }
 });
 
 export const { addRecords } = recordSlice.actions;
-export const getAllRecords = (state) => state.records.records;
+export const getAllRecords = (state) => state.records.display;
 export default recordSlice.reducer;

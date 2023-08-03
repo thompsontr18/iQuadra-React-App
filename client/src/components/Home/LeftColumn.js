2d83717bca7e762f91651d2b055ff8c70ed689ec
Mohith Jothi Kannan <jothiteenu@gmail.com>
2023-08-03 12:55:05 -0400

    column switches

--- client/src/components/Home/LeftColumn.js
+++ client/src/components/Home/LeftColumn.js
@@ -7,6 +7,7 @@ import {
 } from "../../features/recordSlice";
 
 const LeftColumn = () => {
+  const [initialRender, setInitialRender]=useState(true)
   const [formData, setFormData] = useState({
     CollegeSearch: "",
     MajorsSearch: "",
@@ -14,47 +15,41 @@ const LeftColumn = () => {
     PersonSearch: "",
     SortBy: "Colleges",
     SortOrder: "Ascending",
-    Columns:[true]*9
+    Columns:[true, true, true, true, true, true, true, true, true]
   });
   const [clear, setClear] = useState(true);
-  const cols = ["College", "Major", "Course", "Link", "Bus. Email", "Prog. Email", "Bus. Phone", "Prog. Phone", "Details"]
+  const cols = ["College", "Major", "Course", "Link", "BEmail", "PEmail", "BPhone", "PPhone", "Details"]
   const dispatch = useDispatch();
-  const handleInputChange = (event) => {
+  const handleInputChange = (event, ind) => {
     const { name, value } = event.target;
-    setFormData((prevFormData) => ({
-      ...prevFormData,
-      [name]: value,
-    }));
-    setClear(false);
-  };
-  // const handleInputChange = (event, index) => {
-  //   const { name, value } = event.target;
-  //   if (name==="Columns"){
-  //     setFormData((prevFormData)=>({
-  //       ...prevFormData,
-  //       [prevFormData.Columns[index]]:value
-  //     }))
-  //   }
-  //   else{
-  //     setFormData((prevFormData) => ({
-  //       ...prevFormData,
-  //       [name]: value,
-  //     }));
-  //   }
+    if(name==="Columns"){
+      setFormData((prevFormData) => {
+        const newColumns = [...prevFormData.Columns]; // Create a copy of the Columns array
+        newColumns[ind] = !newColumns[ind]; // Toggle the value at the specified index
+        return {
+          ...prevFormData,
+          Columns: newColumns, // Update the Columns array in the state
+        };
+      });
+    }else{
+      setFormData((prevFormData) => ({
+        ...prevFormData,
+        [name]: value,
+      }));
+    }
     
-
-  //   setClear(false);
-  // };
+    setClear(false);
+    setInitialRender(false);
+  };
   useEffect(() => {
-    if (clear) {
+    if (clear && !initialRender) {
       dispatch(fetchOriginalRecords());
-    } else {
+    } else if(!clear & !initialRender){
       dispatch(fetchSearchInput(formData));
     }
-  }, [formData, clear, dispatch]);
+  }, [formData, clear, dispatch, initialRender]);
   const handleClear = (event) => {
     event.preventDefault();
-    console.log(formData);
     setFormData({
       CollegeSearch: "",
       MajorsSearch: "",
@@ -64,7 +59,7 @@ const LeftColumn = () => {
       SortOrder: "Ascending",
       Columns:[true]*9
     });
-    dispatch(fetchOriginalRecords());
+    setClear(true);
   };
   return (
     <div className="leftColumn w-1/4 p-4">
@@ -79,7 +74,7 @@ const LeftColumn = () => {
           placeholder="By College..."
           className="px-4 py-2 border rounded-md border-gray-300 w-full mt-2"
           value={formData.CollegeSearch}
-          onChange={handleInputChange}
+          onChange={(event)=>handleInputChange(event)}
         />
         <input
           name="MajorsSearch"
@@ -87,7 +82,7 @@ const LeftColumn = () => {
           placeholder="By Major..."
           className="px-4 py-2 border rounded-md border-gray-300 w-full mt-2"
           value={formData.MajorsSearch}
-          onChange={handleInputChange}
+          onChange={(event)=>handleInputChange(event)}
         />
         <input
           name="CourseSearch"
@@ -95,7 +90,7 @@ const LeftColumn = () => {
           placeholder="By Course..."
           className="px-4 py-2 border rounded-md border-gray-300 w-full mt-2"
           value={formData.CourseSearch}
-          onChange={handleInputChange}
+          onChange={(event)=>handleInputChange(event)}
         />
         <input
           name="PersonSearch"
@@ -103,7 +98,7 @@ const LeftColumn = () => {
           placeholder="By People..."
           className="px-4 py-2 border rounded-md border-gray-300 w-full mt-2"
           value={formData.PersonSearch}
-          onChange={handleInputChange}
+          onChange={(event)=>handleInputChange(event)}
         />
         <div className="flex items-center justify-center mt-2 py-2">
           <button
@@ -119,7 +114,7 @@ const LeftColumn = () => {
         <select
           name="SortBy"
           value={formData.SortBy}
-          onChange={handleInputChange}
+          onChange={(event)=>handleInputChange(event)}
           className="border rounded border-gray-300 border-r-0 px-2 py-1 rounded-l-full w-2/3"
         >
           <option value="Colleges">College</option>
@@ -129,7 +124,7 @@ const LeftColumn = () => {
         <select
           name="SortOrder"
           value={formData.SortOrder}
-          onChange={handleInputChange}
+          onChange={(event)=>handleInputChange(event)}
           className="border rounded border-gray-300 boreder-l-0 px-2 py-1 rounded-r-full w-1/3"
         >
           <option value="Ascending">A-Z</option>
@@ -142,10 +137,10 @@ const LeftColumn = () => {
         {
           cols.map((col, ind) => (
             <div className="flex items-center mx-1" key={ind}>
-              <span class="flex-shrink-0 pl-[0.15rem] pr-1">{col}</span>
-              <label class="relative inline-flex items-center cursor-pointer ml-auto my-1">
-                <input name="Columns" type="checkbox" class="sr-only peer"/>
-                <div class="w-11 h-6 bg-gray-200 peer-focus:outline-none rounded-full peer peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-[#043d5d]"></div>
+              <span className="flex-shrink-0 pl-[0.15rem] pr-1">{col}</span>
+              <label className="relative inline-flex items-center cursor-pointer ml-auto my-1">
+                <input name="Columns" value={formData.Columns[ind]} type="checkbox" className="sr-only peer" onChange={(event)=>handleInputChange(event, ind)} checked={formData.Columns[ind]}/>
+                <div className="w-11 h-6 bg-gray-50 peer-focus:outline-none rounded-full peer peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-[#043d5d]"></div>
               </label>
             </div>
           ))
